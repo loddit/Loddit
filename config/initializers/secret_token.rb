0af4137f2d4a6e4bd6c337f34f652701fc394919
loddit <loddit@gmail.com>
2012-01-15 01:46:31 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loddit::Application.config.secret_token = 'af3e7c0898e97263098a6ff85277a88002f588f0476881dde66a89a1a23e67cbef3944902a5b399ca32cf9e7325f1f14bbcfef6322623c5001bdabacbfb82619'
